be7f6cae24e2c19a2caf7f625938a9013f59b9d7
IvanVulovic <ivn.vulovic@gmail.com>
2023-04-12 18:23:10 +0200

    Add vite-tsconfig-paths

--- sites/my-site/vite.config.ts
+++ sites/my-site/vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import tsConfigPaths from "vite-tsconfig-paths";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
 });
