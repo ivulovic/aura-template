0f4779b383f951dc3d8dd8590dbc2925aa7fd98c
IvanVulovic <ivn.vulovic@gmail.com>
2023-05-05 22:02:33 +0200

    Added default layout

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ cd sites/my-site && yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+Sources:
+
+Logo: https://www.flaticon.com/free-icon/atom_6412389?term=atomic&page=8&position=42&origin=search&related_id=6412389
--- sites/my-site/src/main.tsx
+++ sites/my-site/src/main.tsx
@@ -1,25 +1,47 @@
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import { translationMessages } from "@my-site/translations";
 
-import App from "./App";
+import DefaultLayout from "./components/Layout/Default";
 import { IntlProvider, defaultLocale } from "./core";
 import store from "./core/redux/utils/createStore";
+import AboutPage from "./pages/About";
+import HomePage from "./pages/Home";
 import { ThemeProvider } from "./providers/Theme";
 
 const container = document.getElementById("root");
 
 const root = createRoot(container!);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <DefaultLayout>
+        <HomePage />
+      </DefaultLayout>
+    ),
+  },
+  {
+    path: "about",
+    element: (
+      <DefaultLayout>
+        <AboutPage />
+      </DefaultLayout>
+    ),
+  },
+]);
+
 root.render(
   <ReduxProvider store={store.store}>
     <PersistGate loading={<></>} persistor={store.persistor}>
       <IntlProvider locale={defaultLocale} defaultLocale={defaultLocale} messages={translationMessages}>
         <ThemeProvider>
-          <App />
+          <RouterProvider router={router} />
         </ThemeProvider>
       </IntlProvider>
     </PersistGate>
